7eb6d90d97f37b9173077a3ab02c8550d36fbfef
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-03-11 23:36:37 +1100

    Used self closing tags instead

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,10 +1,10 @@
 export function Skeleton() {
     return <div className="flex flex-col gap-4 rounded-none md:flex-row md:items-center">
-        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-32 w-full"/>
         <div className="flex flex-row gap-4">
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"/>
+            <div className="skeleton h-4 w-full"/>
+            <div className="skeleton h-4 w-full"/>
         </div>
     </div>;
 }
