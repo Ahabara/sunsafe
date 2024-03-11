256397612b9de8d73750a9768ebe1d2aee80e267
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-03-11 23:41:38 +1100

    added type safety to router

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,36 @@ import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
+
 import ErrorPage from "./components/ErrorPage";
+import MeetTheTeam from "./components/MeetTheTeam";
+import Application from "./components/Application";
 
 const router = createBrowserRouter([
     {
-        path: "/sunsafe",
+        path: "/sunsafe/*",
         element: <App/>,
         errorElement: <ErrorPage/>,
     },
+
+    {
+        path: "sunsafe/teams",
+        element: <MeetTheTeam/>
+    },
+    {
+        path: "sunsafe/app",
+        element: <Application/>,
+        errorElement: <ErrorPage/>,
+
+    },
+
 ]);
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <React.StrictMode>
+            <RouterProvider router={router}/>
+        </React.StrictMode>
     </React.StrictMode>,
 )
